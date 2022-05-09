f42c1a89d7ac285b96815bb382411406f0ae37c6
AliGung <105039130+AliGung@users.noreply.github.com>
2022-05-10 00:11:46 +0800

    Update answer3.js

--- answer3.js
+++ answer3.js
@@ -1,5 +1,4 @@
 function no_recur(n,cur) {
-  if (!(n == parseInt(n))) {   
+  if (!(n == parseInt(n))) {               // If n does not equal to an integer, it will eventually become smaller than 2 
     throw "Invalid input"; }
   return cur + 1 - 1 / n }
-
